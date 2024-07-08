9e02dccdb9a06d2099fb0f2c97d1106160242593
JuanCarlosAlmarioBabilonia <almariobabilonia@gmail.com>
2024-07-08 08:16:18 -0500

    feat: :construction: Ejercicio 03 y 04
    Crea una clase llamada Estudiante que herede de la clase Persona. Agrega una nueva propiedad llamada carrera y un método llamado estudiar que imprima en la consola un mensaje indicando la carrera que está estudiando.
    Crea una instancia de la clase Estudiante llamada estudiante1 y asigna valores a sus propiedades. Luego, llama al método saludar() para que el estudiante imprima su saludo y al método estudiar() para que el estudiante imprima el mensaje de su carrera.

--- main.js
+++ main.js
@@ -43,3 +43,34 @@ let persona1 = new Persona ("Juan", "17", "hombre");
 document.querySelector("#resultado").innerHTML = /*html*/ `
 <h2>${persona1.saludar()}</h2>
 `;
+
+// Crea una clase llamada Estudiante que herede de la clase
+// Persona. Agrega una nueva propiedad llamada carrera y un
+// método llamado estudiar que imprima en la consola un
+// mensaje indicando la carrera que está estudiando.
+
+class Estudiante extends Persona {
+    carrera;
+    constructor(nombre, edad, sexo, carrera){
+        super(nombre, edad, sexo);
+        this.carrera = carrera
+    }
+    get getCarrera(){
+        return this.carrera;
+    }
+    estudiar(){
+        return `${this.saludar()} y estudio ${this.carrera}`;
+    }
+}
+
+// Crea una instancia de la clase Estudiante llamada estudiante1 y
+// asigna valores a sus propiedades. Luego, llama al método
+// saludar() para que el estudiante imprima su saludo y al método
+// estudiar() para que el estudiante imprima el mensaje de su
+// carrera.
+
+let estudiante1 = new Estudiante ("Juan", "17", "hombre", "Artes audiovisuales");
+
+document.querySelector("#resultado").innerHTML = /*html*/ `
+<h2>${estudiante1.estudiar()}</h2>
+`;
